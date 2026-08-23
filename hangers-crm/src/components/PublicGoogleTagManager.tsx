957f95eb12f6f7263417b652a668ec00ccacc35c
kwinagda <kevin.cn@somaiya.edu>
2026-08-23 23:19:10 +0530

    Add marketing Google Analytics tag

--- hangers-crm/src/components/PublicGoogleTagManager.tsx
+++ hangers-crm/src/components/PublicGoogleTagManager.tsx
@@ -5,11 +5,13 @@ import { usePathname } from 'next/navigation'
 
 declare global {
   interface Window {
-    dataLayer?: Array<Record<string, unknown>>
+    dataLayer?: Array<Record<string, unknown> | unknown[]>
+    gtag?: (...args: unknown[]) => void
   }
 }
 
 const GTM_ID = 'GTM-5M5WF5CV'
+const GA_MEASUREMENT_ID = 'G-D23MCHNN38'
 const CRM_PATH_PREFIXES = ['/dashboard']
 const CRM_PATHS = ['/login', '/change-password']
 const MARKETING_HOSTS = new Set(['hangers-cs.com', 'www.hangers-cs.com'])
@@ -38,6 +40,22 @@ export default function PublicGoogleTagManager() {
       script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`
       document.head.appendChild(script)
     }
+
+    if (!document.querySelector(`script[src="https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}"]`)) {
+      const script = document.createElement('script')
+      script.async = true
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
+      document.head.appendChild(script)
+    }
+
+    window.gtag = (...args) => window.dataLayer?.push(args)
+    window.gtag('js', new Date())
+    window.gtag('config', GA_MEASUREMENT_ID, { send_page_view: false })
+    window.gtag('event', 'page_view', {
+      page_path: pathname,
+      page_location: window.location.href,
+      page_title: document.title,
+    })
 
     window.dataLayer.push({
       event: 'page_view',
