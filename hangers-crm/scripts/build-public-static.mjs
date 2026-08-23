432e23fbbc616792f8cbdaf03d97d1110bb58df4
kwinagda <kevin.cn@somaiya.edu>
2026-08-23 23:36:37 +0530

    Keep one Google Analytics tag on public pages

--- hangers-crm/scripts/build-public-static.mjs
+++ hangers-crm/scripts/build-public-static.mjs
@@ -10,9 +10,6 @@ const outDir = path.resolve(process.env.PUBLIC_STATIC_OUT_DIR || 'dist-public');
 // The marketing site is published as static HTML to S3. Keep analytics here so
 // it is present in the initial document on marketing pages only, never in CRM.
 const marketingAnalyticsHead = `
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5M5WF5CV');</script>
-<!-- End Google Tag Manager -->
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-D23MCHNN38"></script>
 <script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-D23MCHNN38');</script>`;
